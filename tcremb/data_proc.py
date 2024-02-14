886a03192b88444cc9c130d594d3a8beab32b653
Yulia Kremlyakova <ykremlyakova@frontend-1-2-13.int.aldan3.itm-rsmu.ru>
2024-02-14 19:18:52 +0300

    add V10-V1000 sampling

--- tcremb/data_proc.py
+++ tcremb/data_proc.py
@@ -70,6 +70,14 @@ def freq_labels_tr_list(label, data_id, data_preped, freqs_list):
         df[k] = df[label].apply(lambda x: x if x in label_high_count_dict[k] else 'other')
     return df
 
+def filter_save_freq_subsets(data, chain, label,samples_n, freq_col_list, dataset_outputs_suf):
+    for i in samples_n:
+        v_output_path = f'{dataset_outputs_suf}_{chain}_V{i}.csv'
+        col = f'{label}_freq_{i}'
+        df = data[data[col]!='other'].drop(freq_col_list,axis=1)
+        df.to_csv(v_output_path, sep='\t',index=False)
+    
+
 ## 10x proc
 def read_barcodes(barcodes_file):
     barcodes = pd.read_csv(barcodes_file, sep = '\t',header = None)
